e580183c1be8b798e78c2614cc6ac4482115e178
SistemasCaseej <githubcase@gmail.com>
2024-11-16 13:16:40 -0300

    Adicionado página de cadastro do usuário

---
+++ pages/register/script.js
@@ -0,0 +1,33 @@
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", (e)=>{
+
+    e.preventDefault()//Evita que o navegador execute as ações padrões
+
+    let name = document.getElementById("name").value;//Esse valor precisa ser único, validação banco de dados
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+
+    let formValues = {
+        name : name, 
+        email : email,
+        password : password, 
+    }
+
+
+    fetch("https://restaurante-backend-iu9b.onrender.com/user/add-user", {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+        },
+        body : JSON.stringify(formValues)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+    form.reset();
+    
+})
